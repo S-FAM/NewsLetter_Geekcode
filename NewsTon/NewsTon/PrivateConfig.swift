feeaddeaef1210212ff0261670aada097d9ce4c2
isgeekcode <ptuguitar@naver.com>
2022-06-20 22:47:50 +0900

    git control

---
+++ NewsTon/NewsTon/PrivateConfig.swift
@@ -0,0 +1,13 @@
+//
+//  PrivateConfig.swift
+//  NewsTon
+//
+//  Created by hyeonseok on 2022/06/20.
+//
+
+import Foundation
+
+class privateInfo {
+    static let clientID: String = "t4OhGel21Q4vXcPk_VxP"
+    static let clientKEY: String = "yr_QKs19Gu"
+}
